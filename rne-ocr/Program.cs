a125dc2607a54f5594c061469bbb297ff060519f
Vlad Bejenariu <vlad.bejenariu@equilobe.com>
2023-09-12 11:05:47 +0300

    add iron ocr. poc
    Signed-off-by: Vlad Bejenariu <vlad.bejenariu@equilobe.com>

---
+++ rne-ocr/Program.cs
@@ -0,0 +1,24 @@
+﻿using IronOcr;
+
+var ocr = new IronTesseract();
+
+ocr.Language = OcrLanguage.EnglishBest;
+// ocr.AddSecondaryLanguage(OcrLanguage.EnglishBest);
+
+
+using (var ocrInput = new OcrInput())
+{
+    ocrInput.AddPdf("8.pdf");
+    // Optionally Apply Filters if needed:
+    // ocrInput.Rotate(90);
+    ocrInput.Sharpen();
+    ocrInput.Binarize();
+    ocrInput.Contrast();
+    ocrInput.Deskew();  // use only if image not straight
+    ocrInput.DeNoise(); // use only if image contains digital noise
+
+    var ocrResult = ocr.Read(ocrInput);
+    Console.WriteLine("FISIER: ");
+    Console.WriteLine(ocrResult.Text);
+    Console.ReadLine();
+}
